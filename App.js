07edcd74131f7e9b86730230c43d4dce3efda9d7
Sharif Jameel <sharifjameel@Sharifs-MacBook-Pro.local>
2021-11-17 16:43:45 +0530

    update

--- App.js
+++ App.js
@@ -1,28 +1,119 @@
 import React from 'react'
 import { 
   View, 
-  // Button, 
   StyleSheet, 
   TextInput, 
   Image, 
   ScrollView, 
-  Text,
   TouchableWithoutFeedback, 
   Keyboard,
-  Platform,
-  SafeAreaView,
+  PermissionsAndroid
 } from 'react-native'
-import { Header, Button } from 'react-native-elements'
+import { Header, Button, Overlay , Text} from 'react-native-elements'
+import RNFetchBlob from 'rn-fetch-blob';
 export default function App() {
   const [image, setImage] = React.useState()
   const [count, setCount] = React.useState()
   const [value, setValue] = React.useState()
+  const [visible, setVisible] = React.useState(false);
+  const [imageurl, setImageUrl] = React.useState()
+
+  const offOverlay = () => {
+    setVisible(false);
+    setImageUrl()
+  };
+  const onOverlay = (OverlayImageUrl) => {
+    setVisible(true)
+    setImageUrl(OverlayImageUrl)
+  }
+
+  const checkPermission = async() => {
+    if (Platform.OS === 'ios') {
+      downloadFile();
+      offOverlay()
+    } else {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+          {
+            title: 'Storage Permission Required',
+            message:
+              'Application needs access to your storage to download File',
+          }
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          // Start downloading
+          downloadFile();
+          offOverlay()
+        } else {
+          // If permission denied then show alert
+          alert('Storage Permission Not Granted');
+        }
+      } catch (err) {
+        // To handle permission related exception
+        alert("++++"+err);
+      }
+    }
+  };
+  const downloadFile = () => {
+   
+    // Get today's date to add the time suffix in filename
+    let date = new Date();
+    // File URL which we want to download
+    let FILE_URL = imageurl;    
+    // Function to get extention of the file url
+    let file_ext = getFileExtention(FILE_URL);
+   
+    file_ext = '.' + file_ext[0];
+   
+    // config: To get response by passing the downloading related options
+    // fs: Root directory path to download
+    const { config, fs } = RNFetchBlob;
+    let RootDir = fs.dirs.PictureDir;
+    let options = {
+      fileCache: true,
+      addAndroidDownloads: {
+        path:
+          RootDir+
+          '/file_' + 
+          Math.floor(date.getTime() + date.getSeconds() / 2) +
+          file_ext,
+        description: 'downloading file...',
+        notification: true,
+        // useDownloadManager works with Android only
+        useDownloadManager: true,   
+      },
+    };
+    config(options)
+      .fetch('GET', FILE_URL)
+      .then(res => {
+        // Alert after successful downloading
+        alert('File Downloaded Successfully.');
+
+      });
+  };
+ 
+  const getFileExtention = fileUrl => {
+    // To get the file extension
+    return /[.]/.exec(fileUrl) ?
+             /[^.]+$/.exec(fileUrl) : undefined;
+  };
+    const Overlays = () => {
+      return (
+        <View style={{width:150, height:150}}>
+          <Overlay isVisible={visible} onBackdropPress={offOverlay}>
+            <Button onPress={checkPermission} style={{padding:5}} title="Download"/>
+          </Overlay>
+        </View>
+        )
+    }
+
    function FetchData() {
     if(image && count) {
       return fetch(`https://bing-image-search1.p.rapidapi.com/images/search?q=${image}&count=${count}`,{
         "method": "GET",
         "headers": {
-          "x-rapidapi-key": "your-api-key",
+          "x-rapidapi-key": "82e25d2101msh0e0b6650251d18bp1ff47cjsnf7a572ee75d9",
           "x-rapidapi-host": "bing-image-search1.p.rapidapi.com"
         }
       })
@@ -51,9 +142,17 @@ export default function App() {
       />
       <ScrollView>
         {value.map((items) => (
+          <TouchableWithoutFeedback onLongPress={() => {
+            onOverlay(items.contentUrl)
+           }}
+           delayLongPress={2000}
+        >
+
         <Image source={{uri: items.contentUrl}} key={items.imageId} style={{ resizeMode: 'cover', width: items.thumbnail.width, height: items.thumbnail.height }}/>
+          </TouchableWithoutFeedback>
         ))}
       </ScrollView>
+      <Overlays/>
       </>
     )
   }
@@ -64,8 +163,8 @@ export default function App() {
       />
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
     <View style={styles.container}>
-      <TextInput placeholder="Image Name *" style={styles.textInput} value={image} onChangeText={setImage}/>
-      <TextInput placeholder="How Much Images You Want *" style={styles.textInput} value={count} onChangeText={setCount}/>
+      <TextInput placeholder="Image Name" style={styles.textInput} value={image} onChangeText={setImage}/>
+      <TextInput placeholder="How Much Images You Want" style={styles.textInput} value={count} onChangeText={setCount}/>
         <Button onPress={() => FetchData()} title="Search Image"/>
     </View>
     </TouchableWithoutFeedback>
